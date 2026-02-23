40ca5e0a8a2424cb322068061c4bb214c5770212
Druhiyat24 <dederuhiyat24@gmail.com>
2026-02-23 16:04:52 +0700

    add bank to excel supplier

--- pages/master/xlsdatacus.php
+++ pages/master/xlsdatacus.php
@@ -16,11 +16,14 @@ header("Expires: 0");
     <th>Alamat</th>
     <th>Area</th>
     <th>Negara</th>
+    <th>Bank Name</th>
+    <th>Bank Account</th>
+    <th>Beneficiary Name</th>
   </thead>
   <tbody>
     <?php 
     $sql="SELECT supplier_code, supplier, alamat, if(area='I','Import/Export',
-      if(area='L','Lokal',if(area='F','Factory',area))) areanya , country
+      if(area='L','Lokal',if(area='F','Factory',area))) areanya , country, bank_name, bank_account, beneficiary_name
       FROM mastersupplier where tipe_sup='C' ORDER BY id_supplier desc";
     tampil_data_tanpa_nourut($sql,5);
     ?>
--- pages/master/xlsdatasup.php
+++ pages/master/xlsdatasup.php
@@ -17,11 +17,14 @@ header("Expires: 0");
     <th>Area</th>
     <th>Negara</th>
     <th>Product</th>
+    <th>Bank Name</th>
+    <th>Bank Account</th>
+    <th>Beneficiary Name</th>
   </thead>
   <tbody>
     <?php 
     $sql="SELECT supplier_code, supplier, alamat, if(area='I','Import/Export',
-      if(area='L','Lokal',if(area='F','Factory',area))) areanya , country, product_name
+      if(area='L','Lokal',if(area='F','Factory',area))) areanya , country, product_name, bank_name, bank_account, beneficiary_name
       FROM mastersupplier where tipe_sup='S' ORDER BY id_supplier desc";
     tampil_data_tanpa_nourut($sql,6);
     ?>
