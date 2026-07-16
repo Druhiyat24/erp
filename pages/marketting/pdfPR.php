1fcb3b61a628b25f98983ae84b2592d606e064fd
alialfi <45139879+alialfi@users.noreply.github.com>
2026-07-16 13:46:31 +0700

    Update pdfPR.php

--- pages/marketting/pdfPR.php
+++ pages/marketting/pdfPR.php
@@ -1,5 +1,9 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
 include_once '../../include/conn.php';
 
 include_once '../forms/fungsi.php';
