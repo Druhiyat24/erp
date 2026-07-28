aba5b53aa067f79e33de0272606feba1146397df
Dede Ruhiyat <95616970+Druhiyat24@users.noreply.github.com>
2026-07-28 13:33:13 +0700

    adjust

--- pages/shp/getDetailsPengajuan.php
+++ pages/shp/getDetailsPengajuan.php
@@ -10,8 +10,8 @@ if (isset($_GET['nomor_aju'])) {
     $nomordaftar = $_GET['nomordaftar'];
 
     // Query untuk mengambil detail pengajuan
-    $sql = "select * from (select kode_dokumen, nomor_aju, SUBSTRING(nomor_aju, -6) AS no_aju, DATE_FORMAT(STR_TO_DATE(SUBSTRING(nomor_aju, 13, 8), '%Y%m%d'), '%Y-%m-%d') AS tgl_aju, LPAD(nomor_daftar,6,0) no_daftar, tanggal_daftar tgl_daftar, status_update_sb from exim_header) a where nomor_aju = '$nomorAju' and kode_dokumen = '$jenisDok' GROUP BY no_aju UNION
-select REPLACE(REPLACE(REPLACE(jenis_dok, '.', ''), ' ', ''),'BC', '') jenis_dok, nomor_aju, nomor_aju no_aju, tanggal_aju, nomor_daftar, tanggal_daftar, 'Active' status from exim_ceisa_manual where status != 'CANCEL' and REPLACE(REPLACE(REPLACE(jenis_dok, '.', ''), ' ', ''),'BC', '') = '$jenisDok' and nomor_aju = '$nomorAju' and nomor_daftar = '$nomordaftar'";
+    $sql = "select * from (select kode_dokumen, nomor_aju, SUBSTRING(nomor_aju, -6) AS no_aju, DATE_FORMAT(STR_TO_DATE(SUBSTRING(nomor_aju, 13, 8), '%Y%m%d'), '%Y-%m-%d') AS tgl_aju, LPAD(nomor_daftar,6,0) no_daftar, tanggal_daftar tgl_daftar, status_update_sb from exim_header) a where tgl_daftar >= '2026-01-01' and nomor_aju = '$nomorAju' and kode_dokumen = '$jenisDok' GROUP BY no_aju UNION
+select REPLACE(REPLACE(REPLACE(jenis_dok, '.', ''), ' ', ''),'BC', '') jenis_dok, nomor_aju, nomor_aju no_aju, tanggal_aju, nomor_daftar, tanggal_daftar, 'Active' status from exim_ceisa_manual where tanggal_aju >= '2026-01-01' and status != 'CANCEL' and REPLACE(REPLACE(REPLACE(jenis_dok, '.', ''), ' ', ''),'BC', '') = '$jenisDok' and nomor_aju = '$nomorAju' and nomor_daftar = '$nomordaftar'";
     $result = mysqli_query($conn_li, $sql);
 
     if ($row = mysqli_fetch_assoc($result)) {
