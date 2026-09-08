b5bdc2c8de59c78784b6729d038c6b41f20ba5d4
alialfi <45139879+alialfi@users.noreply.github.com>
2026-09-08 15:11:07 +0700

    tes query pdf

--- pages/marketting/pdfPR.php
+++ pages/marketting/pdfPR.php
@@ -468,11 +468,11 @@ while($row1 = mysql_fetch_array($rs1))
 
     if($row1['nama_type']=="SKU")
 
-    { $fldgrp=" group by l.sku,a.status,a.id_item,a.id_panel, a.cons "; }
+    { $fldgrp=" group by l.sku,a.status,a.id_item,a.id_panel, a.cons, a.notes "; }
 
     else
 
-    { $fldgrp=" group by a.status,a.id_item,a.id_panel, a.cons "; }
+    { $fldgrp=" group by a.status,a.id_item,a.id_panel, a.cons , a.notes"; }
 
   }
 
@@ -482,7 +482,7 @@ while($row1 = mysql_fetch_array($rs1))
 
     $fldsiz="l.size";
 
-    $fldgrp=" group by a.status,a.id_item,l.size,a.id_panel, a.cons";
+    $fldgrp=" group by a.status,a.id_item,l.size,a.id_panel, a.cons,a.notes";
 
   }
 
@@ -492,7 +492,7 @@ while($row1 = mysql_fetch_array($rs1))
 
     $fldsiz="'All Size'";
 
-    $fldgrp=" group by a.status,a.id_item,l.color,a.id_panel, a.cons";
+    $fldgrp=" group by a.status,a.id_item,l.color,a.id_panel, a.cons, a.notes";
 
   }
 
@@ -504,7 +504,7 @@ while($row1 = mysql_fetch_array($rs1))
 
     $fldsiz="l.size";
 
-    $fldgrp=" group by a.status,a.id_item,l.color,l.size,a.id_panel, a.cons";
+    $fldgrp=" group by a.status,a.id_item,l.color,l.size,a.id_panel, a.cons, a.notes";
 
   }
 
@@ -544,11 +544,11 @@ while($row1_pro = mysql_fetch_array($rs1_pro))
 
     if($row1_pro['nama_type']=="SKU")
 
-    { $fldgrp=" group by l.sku,a.status,a.id_item,a.id_panel, a.cons "; }
+    { $fldgrp=" group by l.sku,a.status,a.id_item,a.id_panel, a.cons , a.notes"; }
 
     else
 
-    { $fldgrp=" group by a.status,a.id_item,a.id_panel, a.cons "; }
+    { $fldgrp=" group by a.status,a.id_item,a.id_panel, a.cons , a.notes"; }
 
   }
 
@@ -558,7 +558,7 @@ while($row1_pro = mysql_fetch_array($rs1_pro))
 
     $fldsiz="l.size";
 
-    $fldgrp=" group by a.status,a.id_item,l.size,a.id_panel, a.cons";
+    $fldgrp=" group by a.status,a.id_item,l.size,a.id_panel, a.cons, a.notes";
 
   }
 
@@ -568,7 +568,7 @@ while($row1_pro = mysql_fetch_array($rs1_pro))
 
     $fldsiz="'All Size'";
 
-    $fldgrp=" group by a.status,a.id_item,l.color,a.id_panel, a.cons";
+    $fldgrp=" group by a.status,a.id_item,l.color,a.id_panel, a.cons, a.notes";
 
   }
 
@@ -580,7 +580,7 @@ while($row1_pro = mysql_fetch_array($rs1_pro))
 
     $fldsiz="l.size";
 
-    $fldgrp=" group by a.status,a.id_item,l.color,l.size,a.id_panel, a.cons";
+    $fldgrp=" group by a.status,a.id_item,l.color,l.size,a.id_panel, a.cons, a.notes";
 
   }
 
